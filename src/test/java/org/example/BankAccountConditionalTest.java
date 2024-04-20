96a300c3433e04e75ebe91d914969523a200a1c3
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2024-04-20 23:04:12 +0200

    Junit Testing - Conditional Test Execution

---
+++ src/test/java/org/example/BankAccountConditionalTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+public class BankAccountConditionalTest {
+
+    @Test
+    @EnabledOnOs({OS.MAC})
+    public void testMac(){
+
+    }
+
+    @Test
+    @EnabledOnOs({OS.WINDOWS})
+    public void testWindows(){
+
+
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_16})
+    public  void testJRE(){
+
+
+    }
+
+    @Test
+    @DisabledOnJre({JRE.JAVA_16})
+    public void testNoJRE16(){
+
+    }
+}
